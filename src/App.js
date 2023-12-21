bbbfc4ebc10d74b4c878bf5553bb7c57c7e13807
majoPS <majito9115@gmail.com>
2023-12-21 00:19:40 -0400

    El ultimo

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { AuthProvider, useAuth } from './AuthContext';
+
+import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
 import IndexPage from './pages/IndexPage';
 import MenuIzquierdo from './pages/MenuIzquierdo';
+import ActualizarEleccion from './pages/ActualizarEleccionModal';
+import PdfConvocatoria from './pages/pdfConvocatoria';
+import VerConvocatoria from './pages/VerConvocatoria';
 import GenerarPdfPreviewPublic from './pages/GenerarPdfPreviewPublic';
-import GenerarPdfListaVotantesPublic from './pages/GenerarPdfListaVotantesPublic'
+import SustitucionDeVocal from './pages/SustitucionDeVocal ';
+import AgregarPermiso from './pages/AgregarPermiso';
+import AsignacionPermiso from './pages/AsignacionPermiso';
+import GenerarPdfListaVotantesPublic from './pages/GenerarPdfListaVotantesPublic';
 
-const PrivateRoute = ({ children }) => {
-  const { authenticated } = useAuth();
 
-  return authenticated ? (
-    <React.Fragment>{children}</React.Fragment>
-  ) : (
-    <Navigate to="/login" replace />
-  );
-};
 
-const App = () => {
+function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<IndexPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/home"
-            element={
-              <PrivateRoute>
-                <MenuIzquierdo />
-              </PrivateRoute>
-            }
-          />
-           <Route path="/pdfPublicado/:id" element={<GenerarPdfPreviewPublic />} />
-        <Route path="/pdfPublicadoLista/:id" element={<GenerarPdfListaVotantesPublic />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IndexPage/>}></Route>
+        <Route path="/login" element={<LoginPage/>}></Route>
+        <Route path="/home" element={<MenuIzquierdo/>}></Route>  
+        <Route path="/actualizarEleccion/:id" element={<ActualizarEleccion />} />
+        <Route path='/PdfConvocatoria/:id' element={<PdfConvocatoria/>}/>
+
+        <Route path="/VerConvocatoria" element={<VerConvocatoria/>}></Route>  
+        <Route path="/pdfPublicado/:id" element={<GenerarPdfPreviewPublic/>} />
+        <Route path="/pdfPublicadoLista/:id" element={<GenerarPdfListaVotantesPublic/>} />
+
+        <Route path="/SustitucionDeVocal" element={<SustitucionDeVocal/>}></Route> 
+
+        <Route path="/AgregarPermiso" element={<AgregarPermiso/>}></Route>
+        <Route path="/AsignacionPermiso" element={<AsignacionPermiso/>}></Route> 
+
+ 
+
+
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
